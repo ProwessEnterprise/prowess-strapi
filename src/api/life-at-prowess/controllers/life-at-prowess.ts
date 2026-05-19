139239dbb393dc5d95ab486c41746067b078eaf9
VasanthaProwess <vasantakumar.r@prowessenterprise.com>
2026-05-19 12:50:44 +0530

    strapi intial setup

---
+++ src/api/life-at-prowess/controllers/life-at-prowess.ts
@@ -0,0 +1,3 @@
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::life-at-prowess.life-at-prowess');
---
+++ src/api/life-at-prowess/routes/life-at-prowess.ts
@@ -0,0 +1,3 @@
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::life-at-prowess.life-at-prowess');
